00b0d70deecbfdc91e62fb4487865659957093ce
Tyson Andre <tysonandre775@hotmail.com>
2017-03-16 15:51:48 -0700

    wip

--- make-rpm.sh
+++ make-rpm.sh
@@ -3,6 +3,7 @@ FOLDER_NAME=memcached-1.4.35-tagged1sflow1
 TAR_NAME=$FOLDER_NAME.tar.gz
 # Clean any files that were built on the host
 make clean || true
+./version.sh
 tar czf /tmp/$TAR_NAME --transform 's,^\./,./'$FOLDER_NAME'/,' . 
 mv /tmp/$TAR_NAME .
 sudo docker build -t memcached-1.4.35 .
